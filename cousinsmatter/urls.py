6633d6083e13e181d4c8e4e47d037b8731aed45c
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-03-17 20:34:35 +0100

    renamed user to account

--- cousinsmatter/urls.py
+++ cousinsmatter/urls.py
@@ -19,22 +19,29 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
+from accounts import views as account_views
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+
+# from django.utils.translation import gettext_lazy as _
+ 
+# admin.site.index_title = _('My Index Title')
+# admin.site.site_header = _('My Site Administration')
+# admin.site.site_title = _('My Site Management')
 
 urlpatterns = [
     path("", include("cm_main.urls")),
-    path("users/", include("users.urls")),
+    path("accounts/", include("accounts.urls")),
     path("members/", include("members.urls")),
     path("news/", include("news.urls")),
     path("galleries/", include("galleries.urls")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     #path("ckeditor/", include("ckeditor_uploader.urls")),
-    #path('password-reset/<uidb64>/<token>/', user_views.reset_password_confirm, name='password_reset_confirm'),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password/reset', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='reset_password'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-	path('password-reset/completed/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-	
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password/reset', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='reset_password'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+	path('password-reset/completed/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
+    path("robots.txt", TemplateView.as_view(template_name="cm_main/robots.txt", content_type="text/plain")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

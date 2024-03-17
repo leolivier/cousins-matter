a644a272b170d825b1466db36b0aaff7cf8d315c
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-03-17 20:36:52 +0100

    translations and signals

--- members/apps.py
+++ members/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class MembersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'members'
+    verbose_name = _("Members")
+
+    def ready(self):
+        import members.signals

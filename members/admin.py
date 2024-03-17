958c9040b4fbac5022242acb3c0a51b13d97dafd
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-03-17 20:36:34 +0100

    added family and address

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Member
+from .models import Member, Family, Address
 
 admin.site.register(Member)
+admin.site.register(Family)
+admin.site.register(Address)

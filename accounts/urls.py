d0adf5ce1d938aab84ac54f7f293dd2e49d28b09
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-03-30 19:50:17 +0100

    cleaned

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('register/', account_views.register, name='register'),
     path('logout/', account_views.logout_account, name='logout'),
     path('password/change/', account_views.change_password, name='change_password'),
-#    path('password/change', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'), name='change_password'),
-#    path('password/changed', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change.html'), name='password_changed_done'),
 ]

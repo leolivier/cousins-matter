399447ff1167834137f5af78af9f0c4a384c740b
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-04-09 14:15:02 +0200

    better default parameters management

--- cm_main/models.py
+++ cm_main/models.py
@@ -4,6 +4,9 @@ from cousinsmatter import settings
 import os
 
 class CousinsMatterParameters(models.Model):
-  home_title = models.CharField(_("Home title"), max_length=128, default=_('Cousins Matter!'))
-  home_content = models.TextField(_("Home content"))
-  home_logo = models.ImageField(_("Home logo"), default=os.path.join(settings.STATIC_URL, 'images', 'cousinsmatter.jpg'))
+  home_title = models.CharField(_("Home title"), max_length=128, 
+                                default=_('Cousins Matter!'))
+  home_content = models.TextField(_("Home content", 
+                                    default=_("No home content yet, please go to <a href='/admin'>admin site</a> and create one.")))
+  home_logo = models.ImageField(_("Home logo"), 
+                                default=os.path.join(settings.STATIC_URL, 'images', 'cousinsmatter.jpg'))
--- cm_main/views.py
+++ cm_main/views.py
@@ -7,16 +7,5 @@ class HomeView(generic.TemplateView):
 	template_name = "cm_main/base.html"
 	def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
 		params = models.CousinsMatterParameters.objects.first()
-		return params.__dict__ if params else {}
-	
-
-# test email
-# from django.core.mail import send_mail
-
-# send_mail(
-#     'Subject',
-#     'Message',
-#     'from@example.com',
-#     ['to@example.com'],
-#     fail_silently=False,
-# )
+		if not params: params = models.CousinsMatterParameters()  # default values
+		return params.__dict__

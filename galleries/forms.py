13da01886efa5da136d2df84a6d72f5c712d180f
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-04-26 18:36:59 +0200

    fixed help text

--- galleries/forms.py
+++ galleries/forms.py
@@ -4,7 +4,7 @@ from .validators import validate_zipfile_extension
 
 class BulkUploadPhotosForm(forms.Form):
     zipfile = forms.FileField(label=_('Zip file'), 
-                              help_text=_('The zip file containing the photos to upload. All folders will be created as galleries and photos in these flders added to the galleries. All photos must be in folders.'),
+                              help_text=_('The zip file containing the photos to upload. All folders will be created as galleries and photos in these folders added to the galleries. All photos must be in folders.'),
                               validators=[validate_zipfile_extension],
                               widget=forms.FileInput(attrs={ 'accept': ".zip"})
                             )

921b0cdca0215413e02f69bb265315d322cf35b2
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-05-09 15:22:05 +0200

    efficient test

---
+++ members/tests/test_directory.py
@@ -0,0 +1,41 @@
+from django.http import FileResponse
+from django.urls import reverse
+from django.utils.translation import gettext as _
+from pprint import pprint
+from .tests_member import MemberTestCase
+from ..views.views_directory import MembersDirectoryView, MembersPrintDirectoryView
+
+class TestMemberDirectory(MemberTestCase):
+	def setUp(self):
+		super().setUp()
+		self.members = []
+		for _ in range(4):
+			self.members.append(self.create_member(self.get_new_member_data()))
+
+	def tearDown(self):
+		for member in self.members:
+			member.delete()
+		super().tearDown()
+	
+	def test_member_directory(self):
+
+		response = self.client.get(reverse('members:directory'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'members/members_directory.html')
+		self.assertIs(response.resolver_match.func.view_class, MembersDirectoryView)
+		# print(response.content)
+		for member in self.members:
+				member_link = f'''
+				<a class="button is-link is-light" href="{reverse("members:detail", args = [member.id])}">
+          <strong>{member.account.get_full_name()}</strong>
+        </a>
+				'''
+				self.assertContains(response, member_link, html=True)
+
+	def test_pdf_generation(self):
+
+		response = self.client.get(reverse('members:print_directory'))
+		self.assertEqual(response.status_code, 200)
+		self.assertIs(response.resolver_match.func.view_class, MembersPrintDirectoryView)
+		self.assertIs(response.__class__, FileResponse)
+		self.assertEqual(response['Content-Type'], 'application/pdf')

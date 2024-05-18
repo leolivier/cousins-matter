0b5c720f15734975ce2d2631bc9d8583c85dd9d7
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-05-18 16:13:16 +0200

    check uploaded file sizes Fixes #41

--- cousinsmatter/utils.py
+++ cousinsmatter/utils.py
@@ -1,6 +1,9 @@
 # util functions for member views
 
+import math
+from django.forms import ValidationError
 from django.shortcuts import redirect
+from django.utils.translation import gettext as _
 
 
 def is_ajax(request):
@@ -12,3 +15,10 @@ def redirect_to_referer(request):
         return redirect(request.META.get('HTTP_REFERER'))
     else:
         return redirect('/')
+
+
+def check_file_size(file, limit):
+    if file.size > limit:
+        limitmb = math.floor(limit*100/(1024*1024))/100
+        sizemb = math.floor(file.size*100/(1024*1024))/100
+        raise ValidationError(_(f"Uploaded file is too big ({sizemb}MB), maximum is {limitmb}MB."))

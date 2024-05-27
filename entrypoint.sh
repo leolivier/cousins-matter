dc571aea143e9d6e0d4986ea051d42e2389755e9
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-05-27 17:47:59 +0200

    add write access to /app to www-data for redis

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export APP_DIR=/app
 . ./prepare-envt.sh || exit 1
+# add write access to /app to www-data for redis
+setfacl -m u:www-data:w /app
 echo "starting $@"   # as provided in the Dockerfile
 exec "$@"

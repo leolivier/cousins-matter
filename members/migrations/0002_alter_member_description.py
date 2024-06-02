9ac1d5e1f2aa850ca18c273b653af59c9c727534
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-06-02 18:05:20 +0200

    manual change of migrations after merge

--- members/migrations/0002_alter_member_description.py
+++ members/migrations/0002_alter_member_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0008_alter_member_hobbies'),
+        ('members', '0001_initial'),
     ]
 
     operations = [

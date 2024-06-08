b6c5ed12165ee0105779aaa9b495cf85cc00f648
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-06-08 17:07:49 +0200

    html validator middleware

--- cousinsmatter/settings.py
+++ cousinsmatter/settings.py
@@ -91,6 +91,7 @@ MIDDLEWARE = [
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  # 'cousinsmatter.htmlvalidator.HtmlValidatorMiddleware',
 ]
 
 ROOT_URLCONF = 'cousinsmatter.urls'

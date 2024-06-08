03509ad992c4556a16ab4d9bd713a44973475361
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-06-08 12:51:34 +0200

    Paginate chat rooms and chat messages

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'chat'
 urlpatterns = [
   path('', views.chat, name='chat'),
+  path('<int:page_num>', views.chat, name='chat_page'),
   path('room', views.new_room, name='new_room'),
   path('room/<str:room_slug>', views.chat_room, name='room'),
+  path('room/<str:room_slug>/<int:page_num>', views.chat_room, name='room_page'),
+  path('test/create_rooms/<int:num_rooms>', views.test_create_rooms, name='test_create_rooms'),
+  path('test/create_messages/<int:num_messages>', views.test_create_messages, name='test_create_messages'),
 ]

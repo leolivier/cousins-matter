4abf96290e11cad3034e8cab78ea36a414756825
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-06-11 17:01:26 +0200

    isolated test urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 
 from . import views
@@ -9,6 +10,9 @@ urlpatterns = [
   path('room', views.new_room, name='new_room'),
   path('room/<str:room_slug>', views.chat_room, name='room'),
   path('room/<str:room_slug>/<int:page_num>', views.chat_room, name='room_page'),
-  path('test/create_rooms/<int:num_rooms>', views.test_create_rooms, name='test_create_rooms'),
-  path('test/create_messages/<int:num_messages>', views.test_create_messages, name='test_create_messages'),
 ]
+if settings.DEBUG:
+  urlpatterns += [
+    path('test/create_rooms/<int:num_rooms>', views.test_create_rooms, name='test_create_rooms'),
+    path('test/create_messages/<int:num_messages>', views.test_create_messages, name='test_create_messages'),
+  ]

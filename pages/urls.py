75918421df6f6590f38233cbeaa26a7162659b7e
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-06-15 18:39:09 +0200

    typo

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,5 @@ app_name = "pages-edit"
 urlpatterns = [
     path("", views.PageListView.as_view(), name="list"),
     path("create", views.PageCreateView.as_view(), name="create"),
-    path("<path:page_url>/", views.PageUpdateView.as_view(), name="update"),
+    path("<path:page_url>", views.PageUpdateView.as_view(), name="update"),
 ]

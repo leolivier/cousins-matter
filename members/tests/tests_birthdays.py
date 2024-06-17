8e76b4cca5bbba06e1e01bce5a69735c768801a9
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-06-17 19:43:57 +0200

    refactored to be usable as mixin in other tests

--- members/tests/tests_birthdays.py
+++ members/tests/tests_birthdays.py
@@ -5,44 +5,32 @@ from .tests_member import MemberTestCase
 from datetime import date, timedelta
 
 
-class TestBirthdays(MemberTestCase):
-  def test_birthdays(self):
-    today = date.today()
-    b_today = date(2000, today.month, today.day)
-    # create a member with birthday = today
+class TestBirthdaysMixin():
+  def get_member_with_bday(self, bday):
+    # create a member with birthday = bday
     new_data = self.get_new_member_data()
-    new_data['birthdate'] = b_today
-    member1 = self.create_member(new_data)
-    # create a member with birthdate = tomorrow - 2 years
-    new_data = self.get_new_member_data()
-    b_tomorrow = date(1998, today.month, today.day) + timedelta(days=1)
-    new_data['birthdate'] = b_tomorrow
-    member2 = self.create_member(new_data)
-    # create a member with birthdate in 2 weeks
-    new_data = self.get_new_member_data()
-    b_2weeks = date(1990, today.month, today.day) + timedelta(weeks=2)
-    new_data['birthdate'] = b_2weeks
-    member3 = self.create_member(new_data)
+    new_data['birthdate'] = bday
+    return self.create_member(new_data)
 
+  def check_birthday(self, member, expected_chain):
     response = self.client.get(reverse("members:birthdays"))
     # pprint(vars(response))
+    self.check_birthday_on_response(response, member, expected_chain)
 
+  def check_birthday_on_response(self, response, member, expected_chain):
     common_chain = '''
       <div class="cell has-background-light-link has-text-right px-0 mx-0">
         <a href="/members/%(member_id)s/">%(first_name)s %(last_name)s</a>
       </div>
     '''
-    self.assertContains(response, common_chain % {"member_id": member1.id,
-                                                  "first_name": member1.first_name,
-                                                  "last_name": member1.last_name}, html=True)
-    self.assertContains(response, common_chain % {"member_id": member2.id,
-                                                  "first_name": member2.first_name,
-                                                  "last_name": member2.last_name}, html=True)
-    self.assertContains(response, common_chain % {"member_id": member3.id,
-                                                  "first_name": member3.first_name,
-                                                  "last_name": member3.last_name}, html=True)
+    self.assertContains(response, common_chain % {"member_id": member.id,
+                                                  "first_name": member.first_name,
+                                                  "last_name": member.last_name}, html=True)
 
-    b_is_today = _("turns %(age)s today, happy birthday!") % {'age': member1.age()}
+    self.assertContains(response, expected_chain, html=True)
+
+  def check_birthday_today(self, member):
+    b_is_today = _("turns %(age)s today, happy birthday!") % {'age': member.age()}
     c_is_today = f'''
       <div class="cell has-background-light-primary px-0 mx-0 has-text-danger">
         {b_is_today}
@@ -51,9 +39,10 @@ class TestBirthdays(MemberTestCase):
         <i class="icon"><span class="mdi mdi-cake-variant-outline"></span></i>
       </div>
     '''
-    self.assertContains(response, c_is_today, html=True)
+    self.check_birthday(member, c_is_today)
 
-    b_is_tomorrow = _("will turn %(age)s tomorrow, happy birthday!") % {'age': member2.age()+1}
+  def check_birthdays_tomorrow(self, member):
+    b_is_tomorrow = _("will turn %(age)s tomorrow, happy birthday!") % {'age': member.age()+1}
     c_is_tomorrow = f'''
       <div class="cell has-background-light-primary px-0 mx-0 has-text-warning">
         {b_is_tomorrow}
@@ -62,13 +51,37 @@ class TestBirthdays(MemberTestCase):
         <i class="icon"><span class="mdi mdi-cake-variant-outline"></span></i>
       </div>
     '''
-    self.assertContains(response, c_is_tomorrow, html=True)
+    self.check_birthday(member, c_is_tomorrow)
 
-    b_date = date_format(member3.next_birthday(), "l d F", use_l10n=True)
-    b_is_after = _("will turn %(age)s on %(birthday)s") % {'age': member3.age()+1, 'birthday': b_date}
+  def check_birthdays_after_tomorrow(self, member):
+    b_date = date_format(member.next_birthday(), "l d F", use_l10n=True)
+    b_is_after = _("will turn %(age)s on %(birthday)s") % {'age': member.age()+1, 'birthday': b_date}
     c_is_after = f'''
       <div class="cell has-background-light-primary px-0 mx-0 has-text-primary">
         {b_is_after}
       </div>
     '''
-    self.assertContains(response, c_is_after, html=True)
+    self.check_birthday(member, c_is_after)
+
+
+class TestBirthdays(TestBirthdaysMixin, MemberTestCase):
+  def test_birthday_today(self):
+    # create a member with birthday = today
+    today = date.today()
+    b_today = date(2000, today.month, today.day)
+    member = self.get_member_with_bday(b_today)
+    self.check_birthday_today(member)
+
+  def test_birthdays_tomorrow(self):
+    today = date.today()
+    # create a member with birthdate = tomorrow - 2 years
+    b_tomorrow = date(1998, today.month, today.day) + timedelta(days=1)
+    member = self.get_member_with_bday(b_tomorrow)
+    self.check_birthdays_tomorrow(member)
+
+  def test_birthdays_after_tomorrow(self):
+    today = date.today()
+    # create a member with birthdate in 2 weeks
+    b_2weeks = date(1990, today.month, today.day) + timedelta(weeks=2)
+    member = self.get_member_with_bday(b_2weeks)
+    self.check_birthdays_after_tomorrow(member)

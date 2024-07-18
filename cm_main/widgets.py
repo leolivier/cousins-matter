829093dbd28657983f64c51d7421d2252f7be23b
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-07-18 13:45:28 +0200

    documentation

--- cm_main/widgets.py
+++ cm_main/widgets.py
@@ -1,5 +1,7 @@
 from django.forms.widgets import Textarea
 
 
+# widgets using this class will have the richtextarea css class
+# see cm_main/templates/cm_main/include-summernote.html for usage
 class RichTextarea(Textarea):
   pass

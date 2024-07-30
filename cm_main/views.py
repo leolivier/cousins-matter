b1a38758ad79af1bb4a661a844069eb1c668ec8a
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-07-30 21:32:00 +0200

    remove the need ofpackage request

--- cm_main/views.py
+++ cm_main/views.py
@@ -1,31 +1,34 @@
 import logging
+from urllib.error import HTTPError
+from urllib.request import urlopen
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user, get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
+from django.core.mail import EmailMultiAlternatives
+from django.http import Http404, StreamingHttpResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.utils.translation import gettext as _
+from django.views import generic
+from wsgiref.util import FileWrapper
+
+import json
+from packaging import version
+
 import os
 import mimetypes
 import tempfile
 import zipfile
-import requests
-from packaging import version
 
-from django.shortcuts import render
-from django.views import generic
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.http import StreamingHttpResponse, Http404
-from django.utils.translation import gettext as _
-from django.contrib.auth import get_user_model, get_user
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.template.loader import render_to_string
-from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
-from wsgiref.util import FileWrapper
-
+from .forms import ContactForm
 from chat.models import ChatMessage, ChatRoom
 from forum.models import Comment, Message, Post
 from galleries.models import Gallery, Photo
 from members.models import Member
-
-from .forms import ContactForm
 
 logger = logging.getLogger(__name__)
 
@@ -139,56 +142,69 @@ def send_zipfile(request):
   return response
 
 
-def get_github_release_version(owner, repo):
+def get_github_release_version(request, owner, repo):
     """
     Retrieves the latest release version of a GitHub repository.
 
+    :param request: Django request object
     :param owner: Repository owner
     :param repo: Repository name
-    :return: Version of latest release or None if not found
+    :return: JSON response with version of latest release or error message if not found
     """
-    url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
-    response = requests.get(url)
 
-    print("version github response:", response)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['tag_name']
-    else:
-        return _("Error: %s") % response.status_code
+    url = f'https://api.github.com/repos/{owner}/{repo}/releases/latest'
+    try:
+      with urlopen(url) as response:
+        data = response.read()
+        json_data = json.loads(data)
+        if 'tag_name' in json_data:
+          return json_data['tag_name']
+        else:
+          messages.error(request, _("Version not found"))
+          return None
+    except HTTPError as e:
+      messages.error(request, e.msg)
+      return None
 
 
 @login_required
 def statistics(request):
   from django.utils.safestring import mark_safe
   admin = Member.objects.filter(is_superuser=True).first()
-  latest_release = get_github_release_version('leolivier', 'cousins-matter')
-  latest_version = version.parse(latest_release)
-  current_version = version.parse(settings.APP_VERSION)
-  if current_version < latest_version:
-    release_warning = _("Your version is not up-to-date.")
-    if request.user.is_superuser:
-      release_warning += "<br>"
-      release_warning += _("Please update it by running the following command:<br><code>./scripts/docker-start.sh -u</code>")
-      release_warning = mark_safe(release_warning)
+  latest_release = get_github_release_version(request, 'leolivier', 'cousins-matter')
+  # print('latest_release', latest_release)
+  if latest_release is not None:
+    latest_version = version.parse(latest_release)
+    current_version = version.parse(settings.APP_VERSION)
+    if current_version < latest_version:
+      release_warning = _("Your version is not up-to-date.")
+      if request.user.is_superuser:
+        release_warning += "<br>"
+        release_warning += _("Please update it by running the following command:<br><code>docker-start.sh -u</code>")
+        release_warning = mark_safe(release_warning)
 
-    latest_release = {
-      'value': latest_release,
-      'warning': release_warning,
-      'icon': 'poop'
-    }
-  elif current_version == latest_version:
-    latest_release = {
-      'value': latest_release,
-      'info': _("Your version is up-to-date."),
-      'icon': 'cool'
-    }
+      latest_release = {
+        'value': latest_release,
+        'warning': release_warning,
+        'icon': 'poop'
+      }
+    elif current_version == latest_version:
+      latest_release = {
+        'value': latest_release,
+        'info': _("Your version is up-to-date."),
+        'icon': 'cool'
+      }
+    else:
+      latest_release = {
+        'value': latest_release,
+        'warning': _("Your version is newer than the latest release (?!?)"),
+        'icon': 'confused'
+      }
   else:
     latest_release = {
-      'value': latest_release,
-      'warning': _("Your version is newer than the latest release (?!?)"),
-      'icon': 'confused'
+      'value': '?',
+      'error': _("Version not found"),
+      'icon': 'poop'
     }
 
   stats = {

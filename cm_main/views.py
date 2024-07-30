47ddd50592ca39cd342f0adab9cc5a1f4c603059
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-07-30 17:06:17 +0200

    add an about page for the site stats Fixes #85

--- cm_main/views.py
+++ cm_main/views.py
@@ -3,6 +3,7 @@ import os
 import mimetypes
 import tempfile
 import zipfile
+import requests
 
 from django.shortcuts import render
 from django.views import generic
@@ -10,14 +11,16 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from wsgiref.util import FileWrapper
 from django.http import StreamingHttpResponse, Http404
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model, get_user
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.template.loader import render_to_string
 from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
+from wsgiref.util import FileWrapper
 
+from chat.models import ChatMessage, ChatRoom
+from forum.models import Comment, Message, Post
 from galleries.models import Gallery, Photo
 from members.models import Member
 
@@ -135,27 +138,70 @@ def send_zipfile(request):
   return response
 
 
+def get_github_release_version(owner, repo):
+    """
+    Retrieves the latest release version of a GitHub repository.
+
+    :param owner: Repository owner
+    :param repo: Repository name
+    :return: Version of latest release or None if not found
+    """
+    url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    response = requests.get(url)
+
+    print("version github response:", response)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['tag_name']
+    else:
+        return _("Error: %s") % response.status_code
+
+
 @login_required
 def statistics(request):
   admin = Member.objects.filter(is_superuser=True).first()
+  latest_release = get_github_release_version('leolivier', 'cousins-matter')
+  if latest_release != settings.APP_VERSION:
+    release_warning = _("Your version is not up-to-date, please update it.")
+    latest_release = {
+      'value': latest_release,
+      'warning': release_warning
+    }
+  else:
+    latest_release = {
+      'value': latest_release,
+      'info': _("Your version is up-to-date.")
+    }
+
   stats = {
+    _('Site'): {
+      _('Site name'): settings.SITE_NAME,
+      _('Site URL'): request.build_absolute_uri('/'),
+      _('Application Version'): settings.APP_VERSION,
+      _('Latest release'): latest_release,
+    },
     _('Members'): {
       _('Total number of members'): Member.objects.count(),
       _('Number of active members'): Member.objects.filter(is_active=True).count(),
       _('Number of managed members'): Member.objects.filter(is_active=False).count(),
     },
-    _('Site'): {
-      _('Site name'): settings.SITE_NAME,
-      _('Site URL'): request.build_absolute_uri('/'),
-      _('Application Version'): settings.APP_VERSION,
-    },
-    _('Administrator'): {
-      _('Administrator full name'): admin.get_full_name(),
-      _('Administrator email'): admin.email,
-    },
     _('Galleries'): {
       _('Number of galleries'): Gallery.objects.count(),
       _('Number of photos'): Photo.objects.count(),
-    }
+    },
+    _("Forums"): {
+      _('Number of posts'): Post.objects.count(),
+      _('Number of post messages'): Message.objects.count(),
+      _('Number of message comments'): Comment.objects.count(),
+    },
+    _("Chats"): {
+      _('Number of chat rooms'): ChatRoom.objects.count(),
+      _('Number of chat messages'): ChatMessage.objects.count(),
+    },
+    _('Administrator'): {
+      _('This site is managed by'): admin.get_full_name(),
+      _('Administrator email'): admin.email,
+    },
   }
   return render(request, 'cm_main/about/site-stats.html', {'stats': stats})

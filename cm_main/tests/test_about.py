fbb21d1a9311cf2d0558ee5c9eff53cf3e6ba9c6
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-08-03 19:40:20 +0200

    new test

---
+++ cm_main/tests/test_about.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+from django.utils.text import slugify
+from django.utils.translation import gettext as _
+
+from members.tests.tests_member_base import MemberTestCase
+
+
+class TestAbout(MemberTestCase):
+  def test_about(self):
+    response = self.client.get(reverse('cm_main:about'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'cm_main/about/site-stats.html')
+    for cat in ['Site', 'Members', 'Galleries', 'Forums', 'Chats', 'Administrator']:
+      category = slugify(_(cat))
+      html = f'<div class="panel-category" id="category-{category}">'
+      self.assertContains(response, html)

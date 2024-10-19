5fa1bb1404caf4893d2c993aeb58abab1e98e7a6
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-10-19 19:39:44 +0200

    refactored to use docker compose

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 container=cousins-matter
 image=ghcr.io/leolivier/$container
 
@@ -32,23 +31,31 @@ Then it will pull the image '$image:<tag>'.
 Afterwards, it will start the image with the proper mounted volumes,the right port and the right command.
 And finally, it will check if a superuser already exists in the database, other wise it will run the command to create it.
 
-IMPORTANT: This script uses jq and either curl or wget. If neither is installed, the script will exit with an error.
 EOF
 	exit 0
 }
 
-if [[ -z $(which jq) ]]; then
-	echo "jq is not installed, please install it and restart the command"
-	exit 1
-fi
+function check_status() {
+ # status=$1; shift
+  status=$?;
+  error=$@
+  if [[ $status != 0 ]]; then
+    echo "Error ($status): $error. Exiting..." >&2
+    exit  $status
+  fi
+}
+
+which docker >/dev/null
+check_status "docker is not installed, please install it and restart the command"
+
+which jq >/dev/null
+check_status "jq is not installed, please install it and restart the command"
 
 tag=''
 directory=$PWD
 port=8000
 update=false
-current_tag=$(docker container ls 2>/dev/null | awk -v name="$container" '$NF == name {split($2, a, ":"); print (a[2] ? a[2] : "latest")}');
 force=false
-be_verbose=true
 
 while getopts ":hufqlt:d:p:" opt; do
 	case $opt in
@@ -58,7 +65,7 @@ while getopts ":hufqlt:d:p:" opt; do
 		t) tag=$OPTARG;;
 		d) directory=$OPTARG;;
 		p) port=$OPTARG;;
-		q) be_verbose=false;;
+		v) be_verbose=false;;
 		f) force=true;;
 		\?) echo "Invalid option -$OPTARG" >&2
 				usage;;
@@ -75,12 +82,27 @@ else
     }
 fi
 
+current_tag=$(docker container ls 2>/dev/null | awk -v name="$container" '$NF == name {split($2, a, ":"); print (a[2] ? a[2] : "latest")}');
+
 if [[ -z $tag ]]; then
 	if [[ -z $current_tag ]]; then
-		verbose "no existing container found, using 'latest' tag"
-		tag='latest'
+		verbose "no existing container found, looking for images..."
+        images=$(docker images 2>/dev/null | awk -v name="$image" '$1 == name {print($2);}');
+		if [[ -n $images ]]; then
+		  verbose "found $image with tag(s) $images"
+		  for img in $images; do
+		    tag=$img
+		    if [[ $img == 'latest' ]]; then
+		      break;
+		    fi
+		  done
+	      verbose "...will use $tag tag"
+		else
+			verbose "No image found, will use 'latest' tag"
+			tag='latest'
+		fi
 	else
-		verbose "reusing existing container tag: $current_tag"
+		verbose "...reusing existing container tag: $current_tag"
 		tag=$current_tag
 	fi
 fi
@@ -112,17 +134,16 @@ if [[ -d $directory ]]; then
 	verbose "directory $directory exists, checking it does contain only allowed files and directories..."
 	for item in "$directory"/*; do
 		basename_item=$(basename "$item")
-		if [[ -f "$item" ]]; then
-			if ! is_in_list "$basename_item" "${allowed_files[@]}"; then
-				echo "$directory seems to contains files that are not allowed (e.g. $item). Only $allowed_files and $allowed_dirs are allowed."
+		if [[ ( -f "$item" && ! $(is_in_list "$basename_item" "${allowed_files[@]}") ) ||
+					( -d "$item" && ! $(is_in_list "$basename_item" "${allowed_dirs[@]}") ) ]]; then
+			echo "$directory seems to contains files that are not allowed (e.g. $item). Only $allowed_files and $allowed_dirs are allowed."
+			read -p "Do you want to continue anyway? [y/N] " -n 1 -r
+			echo	
+			if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 				echo "Please choose another directory or remove the unallowed files & folders."
 				exit 2
-			fi
-		elif [[ -d "$item" ]]; then
-			if ! is_in_list "$basename_item" "${allowed_dirs[@]}"; then
-				echo "$directory seems to contains folders that are not allowed (e.g. $item). Only $allowed_files and $allowed_dirs are allowed."
-				echo "Please choose another directory or remove the unallowed files & folders."
-				exit 2
+			else
+				break
 			fi
 		fi
 	done
@@ -138,7 +159,8 @@ if [[ ! -f .env ]]; then
 	else
 		echo "Neither curl nor wget seems to be installed on your system, cannot download .env.example"
 		echo "Either install curl or wget and restart the command"
-		echo "or download yourself .env.example, rename it to .env and edit it"
+		echo "or download yourself https://raw.githubusercontent.com/leolivier/cousins-matter/main/.env.example,"
+		echo "rename it to .env, edit it and adapt it to your neeeds then restart the command"
 		exit 1
 	fi
 	echo ".env didn't exist, it was created by downloading .env.example from github."
@@ -146,101 +168,40 @@ if [[ ! -f .env ]]; then
 	exit 1
 fi
 
-# Get the local architecture
-arch=$(docker version -f '{{.Server.Arch}}')
+verbose "pulling $tagged_image..."
+tmpf=$(mktemp docker_start.XXXX)
+check_status "Couldn't create temp file"
+docker pull $tagged_image 2>&1 | tee $tmpf
+check_status "Docker pull failed"
 
-# Image digest function
-function get_image_digest() {
-	docker image inspect --format='{{index .RepoDigests 0}}' "$1" 2>/dev/null | cut -d'@' -f2
-}
-
-function pull_image() {
-	tagged_image=$1
-	verbose "pulling $tagged_image..."
-	docker pull $tagged_image
+if grep -q "Status: Downloaded newer image for $tagged_image" $tmpf; then
 	verbose "done"
-}
-
-# Get the remote image manifest
-manifest=$(docker manifest inspect "${tagged_image}")
-
-if [ -z "$manifest" ]; then
-	echo "Unable to retrieve manifest for ${tagged_image}"
-	exit 5
-fi
-
-# Find the digest matching the local architecture
-remote_digest=$(echo "$manifest" | jq -r ".manifests[] | select(.platform.architecture == \"$arch\") | .digest")
-
-if [ -z "$remote_digest" ]; then
-	echo "Remote digest for architecture $arch of ${tagged_image} not found"
-	exit 5
-fi
-
-# Get the local image digest
-local_digest=$(get_image_digest "${tagged_image}")
-
-if [ -z "$local_digest" ]; then
-	verbose "Local image not found: ${tagged_image}"
-	pull_image ${tagged_image}
+	pulled=true
 else
-	# Compare digests
-	verbose "Local	Digest : $local_digest"
-	verbose "Remote Digest : $remote_digest"
+	verbose "You already have the latest version of $tagged_image"
+	pulled=false
+fi
+rm $tmpf
 
-	if [ "$local_digest" != "$remote_digest" ]; then
-		verbose "An update is available for ${tagged_image} (architecture: $arch)"
-		pull_image ${tagged_image}
-	fi
+
+verbose "starting $container"
+docker compose up --pull always --wait -d
+check_status "Docker run failed"
+verbose "started"
+
+verbose "checking if superuser exists..."
+su_exists=$(docker exec $container python manage.py shell -c "from members.models import Member; print(Member.objects.filter(is_superuser=True).exists())" 2>/dev/null)
+check_status "Can't get superuser status"
+
+# keep only last line to remove possible warnings
+su_exists=$(echo "$su_exists" | tail -1)
+
+if [[ $su_exists == 'True' ]]; then
+	verbose "superuser already exists"
+else
+	echo "creating superuser..."
+	docker exec -it $container python manage.py createsuperuser
+	check_status "Can't create super user"
 fi
 
-# check if the container is already running the latest image
-update=true
-if [ -n "$current_tag" ]; then
-	container_image_id=$(docker inspect --format='{{.Image}}' "$container" 2>/dev/null)
-	if [ -z "$container_image_id" ]; then
-		verbose "Container not found : $container"
-	else
-		container_digest=$(get_image_digest "$container_image_id")
-		verbose "Digest of the container: $container_digest"
-
-		if [ "$container_digest" != "$local_digest" ]; then
-			verbose "The container uses an image different from the latest local image."
-		else
-			verbose "The container already uses the current local image."
-			update=false
-		fi
-	fi
-fi
-
-if [[ "$update" == true || $force == true ]]; then
-	verbose "Stopping and removing the container if it exists..."
-	if $(docker ps | grep "$container" > /dev/null); then
-		docker stop $container
-		verbose "old container stopped"
-	fi
-	if $(docker container ls -a | grep "$container" > /dev/null); then
-		docker rm $container
-		verbose "old container removed"
-	fi
-	verbose "starting $container"
-	docker run --name $container -p $port:8000 -d -v ./data:/app/data -v ./.env:/app/.env -v ./media:/app/media $tagged_image
-	verbose "started"
-
-	verbose "Waiting for the container to be ready..."
-	for i in {1..10}; do
-		sleep 1
-		echo -n '.'
-	done
-	verbose
-
-	verbose "checking if superuser exists..."
-	if [[ $(docker exec cousins-matter python manage.py shell -c "from members.models import Member; print(Member.objects.filter(is_superuser=True).exists())" 2>/dev/null) == 'True' ]]; then
-		verbose "superuser already exists"
-	else
-		echo "creating superuser..."
-		docker exec -it $container python manage.py createsuperuser
-	fi
-
-fi
 verbose "done"

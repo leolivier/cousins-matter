a9d09134bea31f53dca7eaf798cb788c16e64213
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-12-20 20:37:34 +0100

    Don't run docker container as root #147

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export APP_DIR=/app
 . ./scripts/prepare-envt.sh || exit 1
-# add full access to /app to www-data for redis and ligthttpd
-setfacl -m u:www-data:rwx /app
 echo "starting $@"   # as provided in the Dockerfile
 exec "$@"	

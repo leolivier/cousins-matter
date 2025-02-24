8642db412a888939891bd69dc29476e963b67c13
Olivier LEVILLAIN <olivier.levillain@free.fr>
2025-02-24 11:42:46 +0100

    managed log file

--- members/management/commands/purge_login_traces.py
+++ members/management/commands/purge_login_traces.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from datetime import timedelta
+from pathlib import Path
+import zipfile
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import timezone
@@ -14,5 +16,20 @@ class Command(BaseCommand):
     a_while_ago = timezone.now() - timedelta(days=settings.LOGIN_HISTORY_PURGE_DAYS)
     # Deletion of logs with a login date prior to LOGIN_HISTORY_PURGE_DAYS days ago
     deleted, _ = LoginTrace.objects.filter(login_at__lt=a_while_ago).delete()
-    self.stdout.write(f"LoginTrace purge complete: {deleted} logs older than "
-                      f"{settings.LOGIN_HISTORY_PURGE_DAYS} days deleted.")
+    logfile = self.get_clean_logfile()
+    with logfile.open("a") as f:
+      f.write(f"LoginTrace purge complete: {deleted} logs older than "
+              f"{settings.LOGIN_HISTORY_PURGE_DAYS} days deleted.\n")
+
+  def get_clean_logfile(self):
+    db_file = Path(settings.DATABASES['default']['NAME'])
+    logfile = db_file.parent() / "cron.log"
+
+    if logfile.exists() and logfile.stat().st_size > 1*1024*1024:  # 1MB
+      self.stdout.write("Logfile too large, compressing...")
+      zipped_logfile = logfile.parent() / f"{logfile.name}.zip"
+      with zipfile.ZipFile(zipped_logfile, "w", zipfile.ZIP_DEFLATED) as z:
+        z.write(logfile)
+      logfile.unlink(missing_ok=True)
+
+    return logfile

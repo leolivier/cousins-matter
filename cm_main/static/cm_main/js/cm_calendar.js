0253bb31cebc5ffdbeb167fe39d58fdcac95fd66
Olivier LEVILLAIN <olivier.levillain@free.fr>
2025-03-29 11:05:05 +0100

    move to jquery

--- cm_main/static/cm_main/js/cm_calendar.js
+++ cm_main/static/cm_main/js/cm_calendar.js
@@ -1,28 +1,31 @@
 // bulma calendar integration
 
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(()=>{
 	lang = navigator.language.substring(0, 2) || "en"
-	fmt = get_format('SHORT_DATE_FORMAT')
-	fmt = fmt.replace('d', 'dd').replace('m', 'MM').replace('y', 'yy')
-	fmt = fmt.replace('Y', 'yyyy').replace('n', 'mm')
+	date_fmt = get_format('SHORT_DATE_FORMAT')
+	date_fmt = date_fmt.replace('d', 'dd').replace('m', 'MM').replace('y', 'yy')
+	date_fmt = date_fmt.replace('Y', 'yyyy').replace('n', 'mm')
 
 	// Initialize all input of date type.
-	options = {'type': "date", "lang": lang, "dateFormat": fmt, 
+	options = {"lang": lang, "dateFormat": date_fmt, "timeFormat": 'HH:mm:ss',
   	"todayLabel": gettext("Today"), "clearLabel": gettext("Clear"),
 		"nowLabel": gettext("Now"), "cancelLabel": gettext("Cancel"),
 		"validateLabel": gettext("Validate"), "displayMode": "inline",
 		"enableYearSwitch" : true, "enableMonthSwitch" : true,
-		"minDate": new Date(1600, 1, 1)
+		"minDate": new Date(1600, 1, 1), displayMode: "dialog"
 	}
-	const calendars = bulmaCalendar.attach('[class~="dateinput"]', options);
+	const date_calendars = [
+		...bulmaCalendar.attach('[class~="dateinput"]', {'type': "date", ...options}),
+		...bulmaCalendar.attach('[class~="datetimeinput"]', {'type': "datetime", ...options})
+	];
 
-	console.log("langnav:", navigator.language, "options:", options)
-	// Loop on each calendar initialized
-	calendars.forEach(calendar => {
-		// Add listener to select event
-		calendar.on('select', date => {
-			console.log(date);
-		});
-	});
+	// console.log("langnav:", navigator.language, "date options:", options)
+	// Loop on each date calendar initialized
+	// date_calendars.forEach(calendar => {
+	// 	// Add listener to select event
+	// 	calendar.on('select', date => {
+	// 		console.log(date);
+	// 	});
+	// });
 });
 

7c6162f7995c4f1eed44f6b70f2df173c5c796de
Olivier LEVILLAIN <olivier.levillain@free.fr>
2025-08-31 14:35:44 +0200

    useless

--- scripts/manual-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export APP_DIR=$PWD
-. ./scripts/prepare-envt.sh || exit 1
-supervisord=$(command -v supervisord)
-[ -z "$supervisord" ] && echo "supervisord not found" && exit 1
-echo "starting supervisord"
-exec sudo $supervisord -c "$APP_DIR/supervisord.conf"

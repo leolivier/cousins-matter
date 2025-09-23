fdabc0c6c9bd2f52b178dd5c8eadd17b9b6a46c4
Olivier LEVILLAIN <olivier.levillain@free.fr>
2025-09-23 19:58:44 +0200

    translated + added path to app

--- scripts/dev/test-cmd.py
+++ scripts/dev/test-cmd.py
@@ -3,20 +3,21 @@ import django
 import os
 import sys
 
-os_name = None
-# initialisation si nécessaire
+# first, setup django
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cousinsmatter.settings')
 django.setup()
+
+# then, get the command
 cmd = sys.argv[1]
-# obtenir le module de la commande
 app_name = get_commands().get(cmd)  # ex: 'auth'
 cmd = load_command_class(app_name, cmd)
 
-# inspecter les arguments ajoutés
+# inspect the arguments added
 if hasattr(cmd, 'add_arguments'):
-    # argparse actions enregistrées sur cmd.parser after create_parser called
+    # argparse actions registered on cmd.parser after create_parser called
     parser = cmd.create_parser('manage.py', cmd)
     for action in parser._actions:
         print(action.option_strings, action.dest, action.help)
 else:
-    print("Pas d'add_arguments ; vérifier la source de la commande.")
+    print("No add_arguments method found on the command.")

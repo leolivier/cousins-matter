2b041f3110fb0135054f7b321c0b21c6934efc0a
Olivier LEVILLAIN <olivier.levillain@free.fr>
2025-10-04 19:20:40 +0200

    new create super user test

---
+++ scripts/tests/test_create_superuser.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this script is expected to be run from the root of the dev environment project
+set -e
+curbranch=$(git branch --show-current)
+if [[ -z "$curbranch" ]]; then
+    echo "Error: git branch --show-current returned an empty string. Please run this script from the root of the dev environment project."
+    exit 1
+fi
+tmpdir=$(mktemp -d)
+python -m scripts.install_cousins_matter -d "$tmpdir" -b "$curbranch"
+cd "$tmpdir"
+docker compose up -d  # as we didn't fill the ADMIN_xxx variables in .env, the container will fail to create the superuser
+# wait for the containers to start
+sleep 20
+python -m scripts.create_superuser

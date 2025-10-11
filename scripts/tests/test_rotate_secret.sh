8f0cfdc7a5df2f1de5dfdb8dfb61c00c20fb93ae
Olivier LEVILLAIN <olivier.levillain@free.fr>
2025-10-11 18:54:10 +0200

    All bash test scripts are migrated to POSIX shell

--- scripts/tests/test_rotate_secret.sh
+++ scripts/tests/test_rotate_secret.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Test rotating secrets in .env
 error() {
@@ -13,27 +13,30 @@ get_key() {
 }
 
 set -e
-[[ -f .env ]] || error 1 "No .env file found in $PWD"
+[ -f .env ] || error 1 "No .env file found in $PWD"
 old_secret=$(get_key SECRET_KEY)
 old_prev_secret=$(get_key PREVIOUS_SECRET_KEYS)
-[[ -z $old_secret ]] && error 1 "No old secret found in $PWD/.env"
+[ -z "$old_secret" ] && error 1 "No old secret found in $PWD/.env"
 grep -q "^PREVIOUS_SECRET_KEYS=" .env || error 1 "No old previous secret found in $PWD/.env"
 # Create a temporary directory
 tmp_dir=$(mktemp -d)
 # Copy the .env file from the project directory
-cp .env $tmp_dir
-script_dir=$(cd $(dirname $0); cd ..; pwd)
-cd $tmp_dir
+cp .env "$tmp_dir"
+script_dir=$(cd "$(dirname "$0")" && cd .. && pwd)
+cd "$tmp_dir"
 # Run the rotate_secret script
-$script_dir/manage_cousins_matter.py rotate-secrets
+python "$script_dir/manage_cousins_matter.py" rotate-secrets || error 1 "rotate-secrets command failed"
 new_secret=$(get_key SECRET_KEY)
 new_prev_secret=$(get_key PREVIOUS_SECRET_KEYS)
 # Check if the secrets have been rotated
-[[ -z $new_secret || -z $new_prev_secret ]] && error 1 "Secrets have not been rotated"
-[[ $new_secret == $old_secret ]] && error 2 "New secret is the same as old secret"
-[[ $new_prev_secret == $old_prev_secret ]] && error 3 "New previous secret is the same as old previous secret"
-[[ ${new_prev_secret} =~ "${old_secret}"$ ]] || error 4 "Old secret is not in the list of new previous secrets"
-cd
-rm -rf $tmp_dir
+[ -z "$new_secret" ] || [ -z "$new_prev_secret" ] && error 1 "Secrets have not been rotated"
+[ "$new_secret" = "$old_secret" ] && error 2 "New secret is the same as old secret"
+[ "$new_prev_secret" = "$old_prev_secret" ] && error 3 "New previous secret is the same as old previous secret"
+case "$new_prev_secret" in
+    *"$old_secret") : ;;
+    *) error 4 "Old secret is not in the list of new previous secrets" ;;
+esac
+cd || true
+rm -rf "$tmp_dir"
 echo "test rotate_secret passed"
 exit 0

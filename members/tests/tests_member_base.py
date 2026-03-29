95709972ddfb2ad0bcd0ec052cd102cbfe324540
Olivier LEVILLAIN <olivier.levillain@free.fr>
2026-03-29 20:05:41 +0200

    cleaned import

--- members/tests/tests_member_base.py
+++ members/tests/tests_member_base.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 import os
-from urllib.parse import urlencode
 from django.conf import settings
 from django.urls import reverse
 from django.contrib.auth import get_user

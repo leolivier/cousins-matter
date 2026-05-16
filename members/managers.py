dbb06d2b4c6892ed9ba19346a72a9cc982fd817d
leolivier <olivier.levillain@free.fr>
2026-05-16 11:11:00 +0200

    feat: implemented postgres similarity search for members filtering

--- members/managers.py
+++ members/managers.py
@@ -1,4 +1,8 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import Value
+from django.db.models.functions import Concat
+from django.db.models.query import QuerySet
 from django.utils.translation import gettext_lazy as _
 
 
@@ -7,7 +11,7 @@ class MemberManager(BaseUserManager):
   Member model manager where first_name, last_name are mandatory
   """
 
-  def create_member(self, username, email, password, first_name, last_name, **extra_fields):
+  def create_member(self, username: str, email: str, password: str, first_name: str, last_name: str, **extra_fields):
     """
     Create and save a user with the given username, email, password, first_name and last_name.
     """
@@ -51,7 +55,7 @@ class MemberManager(BaseUserManager):
     await user.asave()
     return user
 
-  def create_superuser(self, username, email, password, first_name, last_name, **extra_fields):
+  def create_superuser(self, username: str, email: str, password: str, first_name: str, last_name: str, **extra_fields):
     """
     Create and save a SuperUser with the given email and password.
     """
@@ -67,3 +71,21 @@ class MemberManager(BaseUserManager):
 
   def alive(self):
     return self.get_queryset().filter(is_dead=False)
+
+  def dead(self):
+    return self.get_queryset().filter(is_dead=True)
+
+  def fuzzy_search(self, query: str, similarity_threshold: float = 0.2) -> QuerySet:
+    return (
+      self
+      .get_queryset()
+      .annotate(
+        complete_name=Concat("first_name", Value(" "), "last_name"), similarity=TrigramSimilarity("complete_name", query)
+      )
+      .filter(similarity__gt=similarity_threshold)
+      .order_by("-similarity")
+      .distinct()
+    )
+
+  # for m in list(members):
+  #   print(m.full_name, "similarity", m.similarity)
